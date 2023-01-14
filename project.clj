154c93bef1c8554ae85ec51099b6c017c9960c83
Alexandar Guzsvany <git@guzsvany.de>
2023-01-14 01:01:08 +0100

    Day 18 (finished).

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   ;:license {:name "Eclipse Public License"
   ;          :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.priority-map "1.1.0"]]
   :main ^:skip-aot aoc2022.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  ; day18 needed more stack size...
+  :jvm-opts ["-Xss8m"])
--- src/aoc2022/day18.clj
+++ src/aoc2022/day18.clj
@@ -30,13 +30,12 @@
 
 ; part 1
 
-(defn get-empty-neighbors
-  [black-cubes xyz]
-  (remove black-cubes (get-simple-neighbors xyz)))
-
 (defn count-open-sides
   [black-cubes xyz]
-  (count (get-empty-neighbors black-cubes xyz)))
+  (->> xyz
+       get-simple-neighbors
+       (remove black-cubes)
+       count))
 
 (defn part-1
   "..."
@@ -50,115 +49,82 @@
 ; part 2
 
 (defn outside?
-  [^clojure.lang.PersistentVector size [x y z]]
-  (or ;(neg? x) (neg? y) (neg? z)
-      (< x (get size 3 0)) (< y (get size 3 0)) (< z (get size 3 0))
-      (>= x (size 0)) (>= y (size 1)) (>= z (size 2))))
+  [size [x y z]]
+  (or (neg? x) (neg? y) (neg? z)
+      (>= x (:w size)) (>= y (:h size)) (>= z (:d size))))
 
-(defn get-empty-neighbors2
-  [data xyz]
+(defn get-white-neighbors
+  "Like get-white-or-outside-neighbors but without elements which lie
+  outside of size"
+  [black-cubes size xyz]
   (remove (fn [xyz]
-            (or (outside? (:size data) xyz)
-                (get (:black-cubes data) xyz)))
+            (or (outside? size xyz)
+                (get black-cubes xyz)))
           (get-simple-neighbors xyz)))
 
 (defn get-size
+  "Finds the size of the given data.
+  Returns: {:w <width> :h <height> :d <depth>}"
   [data]
   (->> data
-       (reduce (fn[[ax ay az] [x y z]]
-                 [(max ax x) (max ay y) (max az z)])
+       (reduce (fn[[mx my mz] [x y z]]
+                 [(max mx x) (max my y) (max mz z)])
                [0 0 0])
-       (mapv inc)))
-
-#_(get-size (parse-input "res/input/day18_x.txt"))
+       (map inc) ; size := max_value + 1 (for every axs)
+       (interleave [:w :h :d])
+       (apply hash-map)))
 
 (defn let-it-flow
-  "Starting at entry and if white then finds alle connected white cubes and
-  adds them as blue cubes to data.
-  Input/Output: {:black-cubes <set> :size [width height depth] :blue-cubes <set>]"
-  [data entry]
-  (if (or (get-in data [:black-cubes entry]) (get-in data [:blue-cubes entry]))
-    data
-    (let [visited (atom (conj (:blue-cubes data) entry))
-          blue-cubes (tree-seq (constantly true) ; (fn[n] (nil? (@visited n)))
-                               (fn[n]
-                                 (swap! visited conj n)
-                                 (remove @visited (get-empty-neighbors2 data n)))
-                               entry)]
-      (update data :blue-cubes set/union (set blue-cubes)))))
+  "Starting at xyz and if white then finds alle connected white cubes and
+  return them."
+  [black-cubes size blue-cubes xyz]
+  (if (or (black-cubes xyz) (blue-cubes xyz))
+    blue-cubes
+    (let [blue-cubes (conj blue-cubes xyz)]
+      (if-let [children (seq (get-white-neighbors black-cubes size xyz))]
+        (reduce (partial let-it-flow black-cubes size) blue-cubes children)
+        blue-cubes))))
 
-#_(:blue-cubes
+(comment
+  (sort
     (let-it-flow
-      {:black-cubes ; (parse-input "res/input/day18_x.txt")
-       #{ ; front plane
-         [0 0 0] #_[0 0 1] #_[0 0 2]
-         [0 1 0] #_[0 1 1] [0 1 2]
-         [0 2 0] #_[0 2 1] [0 2 2]
-         ; center plane
-         [1 0 0] [1 0 1] [1 0 2]
-         [1 1 0] [1 1 1] [1 1 2]
-         [1 2 0] [1 2 1] [1 2 2]
-         ; back plane
-         [2 0 0] #_[2 0 1] [2 0 2]
-         [2 1 0] #_[2 1 1] [2 1 2]
-         [2 2 0] #_[2 2 1] [2 2 2]}
-       :size [3 3 3]
-       :blue-cubes #{}}
+      #{ ; front plane
+        [0 0 0] #_[0 0 1] #_[0 0 2]
+        [0 1 0] #_[0 1 1]   [0 1 2]
+        [0 2 0] #_[0 2 1]   [0 2 2]
+        ; center plane
+        [1 0 0] #_[1 0 1]   [1 0 2]
+        [1 1 0]   [1 1 1]   [1 1 2]
+        [1 2 0] #_[1 2 1]   [1 2 2]
+        ; back plane
+        [2 0 0] #_[2 0 1]   [2 0 2]
+        [2 1 0] #_[2 1 1]   [2 1 2]
+        [2 2 0] #_[2 2 1]   [2 2 2]}
+      [3 3 3]
+      #{}
       [0 0 1]))
+  )
 
-#_(let [visited (atom #{})
-        data {:root [:ab :ac :ad] :ab [:b1 :b2 :b3] :ac [:c1 :c2 :c3] :ad [:d1 :d2 :d3] :b1 [:ab] }]
-    (tree-seq
-      (fn[n] (get data n)) ; (constantly true)
-      (fn[n] (swap! visited conj n) (remove @visited (get data n)))
-      :root))
-
-(defn find-blue-cubes
-  "Performs a tree-search for blue cubes (=reachable cubes). It starts
-  from every empty cube on all six surfaces."
-  [data]
-  (reduce let-it-flow
-          data
-          (concat
-            (for [y (range (get-in data [:size 1])) z (range (get-in data [:size 2]))] [0 y z])
-            (for [x (range (get-in data [:size 0])) z (range (get-in data [:size 2]))] [x 0 z])
-            (for [x (range (get-in data [:size 0])) y (range (get-in data [:size 1]))] [x y 0]))))
-
-#_(:blue-cubes
-    (find-blue-cubes
-      {:black-cubes ; (parse-input "res/input/day18_x.txt")
-       #{ ; front plane
-         [0 0 0] #_[0 0 1] #_[0 0 2]
-         [0 1 0] [0 1 1] [0 1 2]
-         [0 2 0] #_[0 2 1] [0 2 2]
-         ; center plane
-         [1 0 0] [1 0 1] [1 0 2]
-         [1 1 0] #_[1 1 1] [1 1 2]
-         [1 2 0] [1 2 1] [1 2 2]
-         ; back plane
-         [2 0 0] [2 0 1] [2 0 2]
-         [2 1 0] [2 1 1] [2 1 2]
-         [2 2 0] [2 2 1] [2 2 2]}
-       :size [3 3 3]
-       :blue-cubes #{}}))
-
-(defn get-blue-neighbors
-  [data xyz]
-  (filter (:blue-cubes data) (get-simple-neighbors xyz)))
-
-(defn count-blue-sides
-  [data xyz]
-  (count (get-blue-neighbors data xyz)))
+(defn get-surface-xyz
+  [size]
+  (let [xm (dec (:w size)) ym (dec (:h size)) zm (dec (:h size))]
+    (set
+      (lazy-cat
+        (for [y (range (:h size)) z (range (:d size))] [0 y z])
+        (for [x (range (:w size)) z (range (:d size))] [x 0 z])
+        (for [x (range (:w size)) y (range (:h size))] [x y 0])
+        (for [y (range (:h size)) z (range (:d size))] [xm y z])
+        (for [x (range (:w size)) z (range (:d size))] [x ym z])
+        (for [x (range (:w size)) y (range (:h size))] [x y zm])))))
 
 (defn part-2
   "..."
   []
-  (let [black-cubes (parse-input "res/input/day18_x.txt")
+  (let [black-cubes (parse-input "res/input/day18.txt")
         size (get-size black-cubes)
-        data {:black-cubes black-cubes :size size :blue-cubes #{}}
-        data (find-blue-cubes data)]
-    (pp/pprint data)
+        blue-cubes (reduce (partial let-it-flow black-cubes size) #{} (get-surface-xyz size))
+        outer-surfaces (filter (get-surface-xyz size) black-cubes)]
     (->> black-cubes
-         (map (partial count-blue-sides data))
-         (apply +))))
-; result: 
+         (map #(count (filter blue-cubes (get-simple-neighbors %))))
+         (apply + (count outer-surfaces)))))
+; result: 2508
